51fc1bfea59cc8a72441fd16000b0ca8a76e813e
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-12-22 15:43:55 +0530

    Updated screen view using layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="justify-center items-center flex-1">
-      <Text className="text-red-500 text-center text-5xl text-acccent">Edit app/index.tsx to edit this screen. Test</Text>
+      <Text className="text-center text-xl text-acccent">Edit app/index.tsx to edit this screen. Test</Text>
     </View>
   );
 }
