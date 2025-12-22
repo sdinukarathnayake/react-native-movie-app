795fd30a5f6f65d0b11429ee650a31715bae3243
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-12-22 12:17:28 +0530

    Added custom colour theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100:"#221F3D",
+          200: "#1B1737",
+        },
+        acccent: "#AB8BFF"
+      }
+    },
   },
   plugins: [],
 }
