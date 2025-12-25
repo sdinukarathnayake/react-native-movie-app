f23136cd1af2c79b0d32822ecbb971a0debff386
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-12-25 14:35:22 +0530

    Added search bar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
+import { SearchBar } from "react-native-screens";
 
 export default function Index() {
     return (
@@ -9,6 +10,10 @@ export default function Index() {
 
             <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
                 <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+                <View className="flex-1 mt-5">
+                    <SearchBar />
+                </View>
             </ScrollView>
         </View>
     );
